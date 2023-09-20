7acd235dfd5d2b01ebd4d7cc0a1c07f3c19f04a5
Itishree2000 <127124225+Itishree2000@users.noreply.github.com>
2023-09-20 19:38:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
+function removeArticles(bandName) {
+    return bandName.replace(/^(a |an |the )/i, "").trim();
+  }
 
+  // Sort the band names without articles
+  bandNames.sort((a, b) => {
+    const bandNameA = removeArticles(a);
+    const bandNameB = removeArticles(b);
+    return bandNameA.localeCompare(bandNameB);
+  });
+
+  // Get the ul element with id 'bands'
+  const bandList = document.getElementById("bands");
+
+  // Create and append list items for each band name
+  bandNames.forEach((bandName) => {
+    const li = document.createElement("li");
+    li.textContent = bandName;
+    bandList.appendChild(li);
+  });
+});
